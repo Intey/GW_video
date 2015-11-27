21f747233c7f1989249474a2bf407e35260db8d5
Intey <ziexe0@gmail.com>
2015-11-27 22:19:03 +0000

    Mock

--- harvester.js
+++ harvester.js
@@ -9,13 +9,13 @@ var linkGetter = require('./linkGetter.js')
 
 // when 1 link fetched, call given function
 linkGetter.onEach(function(link, filename) { 
-  request(link).pipe(fs.createWriteStream(filename));
-  return "OK!";
+	request(link).pipe(fs.createWriteStream(filename));
+	return "OK!";
 });
 
 // when all 'tasks' is done - call function
 linkGetter.onAll(function(links, names) {
-  console.log(links);
-  console.log(names);
+	console.log(links);
+	console.log(names);
 });
 
--- linkGetter.js
+++ linkGetter.js
@@ -14,37 +14,40 @@ var test_event_url = events_page_url + '163';
 
 
 function onAuthed(e, res, body) {      
-  var cookies = res['headers']['set-cookie'][0];
-  var options = { url: test_event_url, headers: { 'Cookie': cookies } };
+	var cookies = res['headers']['set-cookie'][0];
+	var options = { url: test_event_url, headers: { 'Cookie': cookies } };
 
-  request(options, function(e, res, body) {
-    var $ = cheerio.load(body);
-    console.log($("#video>source").attr('src'));
-  });
+	request(options, function(e, res, body) {
+		var $ = cheerio.load(body);
+		console.log($("#video>source").attr('src'));
+	});
 }
 
 function login(token) {
-  var data = JSON.parse(fs.readFileSync('adata', 'utf-8'));
-  request.post( {
-    url: url+'login', 
-    form: {
-      'authenticity_token': token, 
-      'user[email]': data.username, 
-      'user[password]': data.password, 
-      'user[remember_me]': 0
-    } },
-    onAuthed);
+	var data = JSON.parse(fs.readFileSync('adata', 'utf-8'));
+	request.post( {
+		url: url+'login', 
+		form: {
+			'authenticity_token': token, 
+			'user[email]': data.username, 
+			'user[password]': data.password, 
+				'user[remember_me]': 0
+		} },
+		onAuthed);
 }
 
 function onTokenParse(e, res, body) {
-      var $ = cheerio.load(body);
-      var token = $("input[name='authenticity_token']").attr('value');
-      login(token);
+	var $ = cheerio.load(body);
+	var token = $("input[name='authenticity_token']").attr('value');
+	login(token);
 }
 
 // request(url+'login', onTokenParse);
 
-module.exports.run = function(callback) {
-  console.log(callback());
+module.exports.onEach = function(callback) {
+	console.log(callback());
+}
+module.exports.onAll = function(callback) {
+	console.log(callback());
 }
 

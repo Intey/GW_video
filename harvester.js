ee07eff85fbcce85664653930b2d74a4cd49e448
Intey <ziexe0@gmail.com>
2015-11-27 22:07:22 +0000

    Determine interface, from usage.

--- harvester.js
+++ harvester.js
@@ -1,7 +1,16 @@
+var fs = require('fs');
 var request = require('request');
-var cheerio = require('cheerio');
-var parser = require('./parser.js')
+var linkGetter = require('./linkGetter.js')
 
-parser.run(function() { return "Opa"; });
+// when 1 link fetched, call given function
+linkGetter.onEach(function(link, filename) { 
+  request(link).pipe(fs.fs.createWriteStream(filename));
+  return "OK!";
+});
 
+// when all 'tasks' is done - call function
+linkGetter.onAll(function(links, names) {
+  console.log(links);
+  console.log(names);
+});
 

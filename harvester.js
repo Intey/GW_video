619351af578c4df9599992547e1973190d9aa9e8
Intey <ziexe0@gmail.com>
2015-11-30 21:37:43 +0000

    Add downloading.

--- harvester.js
+++ harvester.js
@@ -1,22 +1,42 @@
 //
-// Entry point. Client of linkGetter.js. Define function that was called when 
+// Entry point. Client of linkGetter.js. Define function that was called when
 // links ready. Should define funcs to download files.
 //
 
-
 var fs = require('fs');
 var request = require('request');
-var linkGetter = require('./linkGetter.js')
+var linkGetter = require('./linkGetter.js');
 
-// when 1 link fetched, call given function
-linkGetter.onEach(function(link, filename) { 
-	console.log(link);
-	// request(link).pipe(fs.createWriteStream(filename));
+var splitter = ';';
+
+var exists_videos = [];
+var dest_path = "videos";
+
+function processLink(link, event_number, video_name) {
+	if ( exists_videos.indexOf(event_number) == -1 ) {
+		console.log("New video. Start downloading" + video_name);
+		request(link).pipe( 
+				fs.createWriteStream(dest_path + '/' + video_name + '.mp4') 
+				);
+
+		fs.appendFile('links', event_number + splitter + link + '\n');
+	} 
+	else { console.log('Exists, so skip:' + video_name ); }
+}
+
+// main processing. In readFile, couze its async. It's ALL async. >_<
+fs.readFile('links', 'utf-8', function(err, data) { 
+	if (err) throw err;
+	var rows = data.split('\n');
+	rows.map(function(e) { 
+		var num = parseInt( e.split(splitter)[0] );
+		if ( ! isNaN(num) ) {
+			exists_videos.push(num); 
+		}
+	});
+	console.log("Exists videos: " + exists_videos);
+	console.log("Start searching...");
+	linkGetter.onEach(processLink);
 });
 
-// when all 'tasks' is done - call function
-// linkGetter.onAll(function(links, names) {
-// 	console.log(links);
-// 	console.log(names);
-// });
 
